b4af4c3507c3e9f35113c16a65ea2464d4699946
Travis Kane <kevinthomaskane@gmail.com>
2018-11-24 12:19:09 -0800

    change title

---
+++ client/build/precache-manifest.23e94b92940cf8ba06bfa11cfe6c5aae.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "ed45666e4dacd9baa4be",
+    "url": "/static/js/main.ed45666e.chunk.js"
+  },
+  {
+    "revision": "a010678d8376de3d1acc",
+    "url": "/static/js/1.a010678d.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "70d9859980119b6c4f0b1cec6d2e3e76",
+    "url": "/index.html"
+  }
+];
