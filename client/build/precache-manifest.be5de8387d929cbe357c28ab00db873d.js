d8ed73e3ac62cfbb8cf2bd1d6b873bbfc1822501
Travis Kane <kevinthomaskane@gmail.com>
2018-12-01 17:25:47 -0800

    Run build script

---
+++ client/build/precache-manifest.be5de8387d929cbe357c28ab00db873d.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "4b933366ded496aa733a",
+    "url": "/static/js/main.4b933366.chunk.js"
+  },
+  {
+    "revision": "273e313bac42642e7b6a",
+    "url": "/static/js/1.273e313b.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "bfb1ef123b12cdfe6783fbe9873b0fb1",
+    "url": "/index.html"
+  }
+];
